0d289eb1de33ab4bfdc3cab69cbb701dfdaddaf6
Nehesh Sawarkar <nehesh07@gmail.com>
2024-02-22 13:03:49 +0530

    File Handling

--- Practical/P9.cpp
+++ Practical/P9.cpp
@@ -2,13 +2,11 @@
 #include <fstream>
 using namespace std;
 
-
-
 int main()
 {
-    ofstream even, odd;
-    even.open("d:/Nehesh_48/OOP/Practical/Result/P9_even.txt");
-    odd.open("d:/Nehesh_48/OOP/Practical/Result/P9_odd.txt");
+    fstream even, odd;
+    even.open("d:/Nehesh_48/OOP/Practical/Result/P9_even.txt", ios::in | ios::out);
+    odd.open("d:/Nehesh_48/OOP/Practical/Result/P9_odd.txt", ios::in | ios::out);
 
     for(int i = 1; i <= 100; i++)
     {
@@ -18,26 +16,23 @@ int main()
             odd << i << " ";
     }
 
+    int data;
+    even.seekg(0, ios::beg);
+    odd.seekg(0, ios::beg);
+
+    cout << "Contents of Even.txt :\n";
+    while(even)
+    {
+        even >> data;
+        cout << data << " ";
+    }
+    cout << "\nContents of Odd.txt :\n";
+    while(odd)
+    {
+        odd >> data;
+        cout << data << " ";
+    }
+
     even.close();
     odd.close();
-
-    int data;
-    ifstream e, o;
-    e.open("d:/Nehesh_48/OOP/Practical/Result/P9_even.txt");
-    o.open("d:/Nehesh_48/OOP/Practical/Result/P9_odd.txt");
-    cout << "Contents of Even.txt :\n";
-    while(e)
-    {
-        e >> data;
-        cout << data << " ";
-    }
-    cout << "Contents of Odd.txt :\n";
-    while(o)
-    {
-        o >> data;
-        cout << data << " ";
-    }
-
-    e.close();
-    o.close();
 }
